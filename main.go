a6706880bebfdcac2cd3395622dd488ed36f5b6e
Maxim Belozerov <avismortes@yandex.ru>
2022-03-24 16:44:53 +0300

    правки

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 func main() {
 	//http.HandleFunc("/transactions/129562341-baf56f8476", handler3)
 	//http.HandleFunc("/ctp/api/checkouts/e3596a225bf79b111ec885a0f07e5f465416b91248c89f892e9f984cdc7681a1", handler2)
-	http.HandleFunc("/", handler1)
-	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/rss/category/now.json", getInfo)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -21,67 +21,33 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
-func handler1(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Println(r.Method, r.UserAgent(), r.RemoteAddr, time.Now().Format(time.RFC3339))
-	file, err := os.Open("responce.txt")
+func getData(name string) []byte {
+	file, err := os.Open(name)
 	defer file.Close()
-
 	data := make([]byte, 64)
-
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	time.Sleep(10 * time.Second)
 	for {
 		n, err := file.Read(data)
 		if err == io.EOF { // если конец файла
 			break
 		}
-		fmt.Fprintf(w, "%s", string(data[:n]))
+		return data[:n]
 	}
+	return data
 }
 
-func handler2(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(r.Method, r.UserAgent(), r.RemoteAddr, time.Now().Format(time.RFC3339))
-	file, err := os.Open("responce2.txt")
-	defer file.Close()
-
-	data := make([]byte, 64)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	for {
-		n, err := file.Read(data)
-		if err == io.EOF { // если конец файла
-			break
-		}
-		fmt.Fprintf(w, "%s", string(data[:n]))
-	}
+	data := getData("response.txt")
+	fmt.Fprintf(w, "%s", string(data))
 }
 
-func handler3(w http.ResponseWriter, r *http.Request) {
-
+func getInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Method, r.UserAgent(), r.RemoteAddr, time.Now().Format(time.RFC3339))
-	file, err := os.Open("responce3.txt")
-	defer file.Close()
-
-	data := make([]byte, 64)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	for {
-		n, err := file.Read(data)
-		if err == io.EOF { // если конец файла
-			break
-		}
-		fmt.Fprintf(w, "%s", string(data[:n]))
-	}
+	data := getData("getInfo.txt")
+	fmt.Fprintf(w, "%s", string(data))
 }
